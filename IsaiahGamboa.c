34b5d1297f11da5a34f88410ff8f98ee69d4822f
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-02-28 13:19:24 -0800

    gui fixes

--- IsaiahGamboa.c
+++ IsaiahGamboa.c
@@ -1,54 +1,59 @@
 #include <stdio.h>
 
-#define MAX_CLASSES 100
+#define CLASSES 100
 
 //Global Variables
+int num_classes = 0; //classes max is 3
+double cpi[CLASSES]; // cpi
+double instruction_count[CLASSES]; // instructions
+double clock_rate; //clock rate
 
-int num_classes = 0;
-double cpi[MAX_CLASSES];
-double instruction_count[MAX_CLASSES];
-double clock_rate;
+void enter_params() {			//need to make inputs for option 1
 
-void enter_params() {			//selection one: entering the parameters
-
-	printf("Enter the number of instructions: ");
+	printf("Enter the number of instructions: "); //number of intructions
 	scanf("%d", &num_classes);
 
-	printf("Enter the frequency of the machine (MHz): ");
+	printf("Enter the frequency of the machine (MHz): "); //machine frequency
 	scanf("%lf", &clock_rate);
 
 	for (int i = 0; i < num_classes; i++) {
-		printf("Enter CPI of class %d: ", i +1);
+		printf("Enter CPI of class %d: ", i +1); //CPI entries
 		scanf("%lf", &cpi[i]);
         
-        printf("Enter instruction count of class %d (millions): ", i + 1);
+        printf("Enter instruction count of class %d (millions): ", i + 1); //instruction coun of class 1 thru 3
         scanf("%lf", &instruction_count[i]);
     }
 }
-
-void print_input_table() {
+// use format %.2f for printf statements
+void print_input_table() { //need to make table of input values for option 2
     printf("\n-----------------------\n");
     printf("| Class | CPI  | Count|\n");
     printf("-----------------------\n");
-    for (int i = 0; i < num_classes; i++) {
-        printf("| %5d | %.2f | %.2f |\n", i + 1, cpi[i], instruction_count[i]);
+    for (int i = 0; i < num_classes; i++) { //have a loop to see the number of classes
+        printf("| %3d | %.2f | %.2f |\n", i + 1, cpi[i], instruction_count[i]); //make things fit nicely in table
     }
     printf("-----------------------\n");
 }
 
-void print_performance_table() {
-    double total_cycles = 0;
+void print_performance_table() { //need to make performance table for option 3
+    //need calculations for Average CPI, CPU time, and MIPS
+    //will need instructions and cycles for calculations
+    double total_cycles = 0; 
     double total_instructions = 0;
     
+    //need to make sure option three covers as many classes were inputed
     for (int i = 0; i < num_classes; i++) {
         total_cycles += cpi[i] * instruction_count[i];
         total_instructions += instruction_count[i];
     }
     
+    //need to code the calculations for each value
+    //reference lecture slides for calculations
     double avg_cpi = total_cycles / total_instructions;
-    double cpu_time = (total_instructions * avg_cpi) / clock_rate * 1000; // Convert to ms
+    double cpu_time = (total_instructions * avg_cpi) / clock_rate * 1000; //need to convert to ms
     double mips = clock_rate / avg_cpi;
     
+    //Make the table of performance
     printf("\n-------------------------\n");
     printf("| Performance  | Value  |\n");
     printf("-------------------------\n");
@@ -58,7 +63,7 @@ void print_performance_table() {
     printf("-------------------------\n");
 }
 
-int main() {
+int main() {        //need to make option selector for this to work
     int choice;
     do {
         printf("\nPerformance assessment:\n");
@@ -70,6 +75,7 @@ int main() {
         printf("Enter selection: ");
         scanf("%d", &choice);
         
+        //fill in the different cases with 
         switch (choice) {
             case 1:
                 enter_params();
@@ -86,7 +92,7 @@ int main() {
             default:
                 printf("Invalid choice. Please try again.\n");
         }
-    } while (choice != 4);
+    } while (choice != 4); //make sure it only remains 4 options
     
     return 0;
 }
